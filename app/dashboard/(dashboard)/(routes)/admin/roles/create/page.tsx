97cfd9f74ad450efd2f587ce91cfcf7a8b4be747
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-09-23 22:47:21 +0100

    Update page.tsx

--- app/dashboard/(dashboard)/(routes)/admin/roles/create/page.tsx
+++ app/dashboard/(dashboard)/(routes)/admin/roles/create/page.tsx
@@ -1,113 +1,109 @@
-'use client'
+"use client";
 
 import * as z from "zod";
-import { useAuth, useOrganization } from '@clerk/nextjs'
-import { OrganizationCustomRoleKey } from '@clerk/types'
-import { useEffect, useRef, useState } from 'react'
-import SelectRole from '../_components/select-role'
-import { Button } from '@/components/ui/button'
-import toast from 'react-hot-toast'
-import { isClerkAPIResponseError } from '@clerk/nextjs'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useAuth, useOrganization } from "@clerk/nextjs";
+import { OrganizationCustomRoleKey } from "@clerk/types";
+import { useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+import { isClerkAPIResponseError } from "@clerk/nextjs";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useRouter } from "next/navigation";
 
 interface InviteMemberProps {
-  email: string | undefined; 
+  email: string | undefined;
   role: OrganizationCustomRoleKey | undefined[];
 }
 
-export const OrgInvitationsParams = {
-  invitations: {
-    pageSize: 5,
-    keepPreviousData: true,
-  },
-}
-
 const formSchema = z.object({
-  email: z.
-    string().
-    min(1, {
-      message: "Email is required",
-    }).
-    email("This is not a valid email.").
-    trim(),
-  role: z.string({
-    required_error: "Please select a role to display.",
-  })
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email("This is not a valid email.")
+    .trim(),
+  role: z.string({ required_error: "Please select a role to display." }),
 });
 
 // Form to invite a new member to the organization.
-export const InviteMember = ({ email, role } :InviteMemberProps) => {
-  const {orgRole} = useAuth();
-  const { isLoaded, organization, invitations } = useOrganization(OrgInvitationsParams)
+const InviteMember = ({ email, role }: InviteMemberProps) => {
+  const { orgRole } = useAuth();
+  const { isLoaded, organization } = useOrganization({
+    invitations: { pageSize: 5, keepPreviousData: true },
+  });
   const router = useRouter();
-  const [disabled, setDisabled] = useState(false)
-  const [fetchedRoles, setRoles] = useState<OrganizationCustomRoleKey[]>([])
-  const isPopulated = useRef(false)
+  const [disabled, setDisabled] = useState(false);
+  const [fetchedRoles, setRoles] = useState<OrganizationCustomRoleKey[]>([]);
+  const isPopulated = useRef(false);
 
   useEffect(() => {
-    if (isPopulated.current) return
-    organization
-      ?.getRoles({
-        pageSize: 20,
-        initialPage: 1,
-      })
-      .then((res) => {
-        isPopulated.current = true
-        setRoles(res.data.map((roles) => roles.key as OrganizationCustomRoleKey))
-      })
-  }, [organization?.id])
+    if (isPopulated.current) return;
+    organization?.getRoles({ pageSize: 20, initialPage: 1 }).then((res) => {
+      isPopulated.current = true;
+      setRoles(res.data.map((role) => role.key as OrganizationCustomRoleKey));
+    });
+  }, [organization?.id]);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: " ",
       role: " ",
-    }
-  })
-  const {isSubmitting, isValid} = form.formState;
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
 
   if (!isLoaded || !organization) {
-    return <>Loading</>
+    return <>Loading</>;
   }
 
-  const onSubmit = async(values: z.infer<typeof formSchema>) => {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await organization.inviteMember({
         emailAddress: values.email,
-        role: values.role
-      })
+        role: values.role,
+      });
       toast.success("Please check your email to accept invitation");
-      router.push('/dashboard/admin/invitations');
-    } catch(err: any) {
-        if (isClerkAPIResponseError(err)) toast.error(err.errors[0].message)
-  }
-}
+      router.push("/dashboard/admin/invitations");
+    } catch (err: any) {
+      if (isClerkAPIResponseError(err)) toast.error(err.errors[0].message);
+    }
+  };
 
-
-  if (fetchedRoles.length === 0) return null
+  if (fetchedRoles.length === 0) return null;
 
   return (
     <div className="max-w-5xl mx-auto flex md:items-center md:justify-left h-full p-6">
       <div>
-        <h1 className="text-2xl">
-          Invite member
-        </h1>
+        <h1 className="text-2xl">Invite member</h1>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 mt-8">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
             <FormField
               control={form.control}
               name="email"
-              render={({field}) => (
+              render={({ field }) => (
                 <FormItem>
-                  <FormLabel>
-                    Email
-                  </FormLabel>
+                  <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input
                       disabled
@@ -115,7 +111,7 @@ export const InviteMember = ({ email, role } :InviteMemberProps) => {
                       {...field}
                     />
                   </FormControl>
-                  <FormMessage/>
+                  <FormMessage />
                 </FormItem>
               )}
             />
@@ -123,25 +119,39 @@ export const InviteMember = ({ email, role } :InviteMemberProps) => {
             <FormField
               control={form.control}
               name="role"
-              render={({field}) => (
+              render={({ field }) => (
                 <FormItem>
-                <FormLabel>Role</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select role" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                  {fetchedRoles?.map((roleKey) => (
-                    <SelectItem key={roleKey}value={roleKey} disabled = {orgRole === 'org:admin' && roleKey.includes('org:super_admin')}>
-                      {roleKey === "org:admin" ? "Admin" : roleKey === "org:member" ? "Member" : "Super admin"}
-                    </SelectItem>
-                  ))}
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
+                  <FormLabel>Role</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select role" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {fetchedRoles?.map((roleKey) => (
+                        <SelectItem
+                          key={roleKey}
+                          value={roleKey}
+                          disabled={
+                            orgRole === "org:admin" &&
+                            roleKey.includes("org:super_admin")
+                          }
+                        >
+                          {roleKey === "org:admin"
+                            ? "Admin"
+                            : roleKey === "org:member"
+                            ? "Member"
+                            : "Super admin"}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
               )}
             />
             <Button type="submit" disabled={!isValid || isSubmitting}>
@@ -151,9 +161,7 @@ export const InviteMember = ({ email, role } :InviteMemberProps) => {
         </Form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default InviteMember;
-
-<SelectRole/>
