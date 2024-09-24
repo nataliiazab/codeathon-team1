6356570610e3215fa521e0317fa64e1b29751f2c
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-09-24 20:35:38 +0100

    delete googlemap-places.tsx

--- app/dashboard/(dashboard)/(routes)/admin/donors/[donorId]/page.tsx
+++ app/dashboard/(dashboard)/(routes)/admin/donors/[donorId]/page.tsx
@@ -6,15 +6,10 @@ import { NameForm } from "./_components/title-form";
 import { EmailForm } from "./_components/email-form";
 import { GooglemapPlaces } from "./_components/googlemap-places";
 
-
-const DonorIdPage = async ({
-  params
-}: {
-  params: { donorId: string }
-}) => {
+const DonorIdPage = async ({ params }: { params: { donorId: string } }) => {
   const { userId } = auth();
 
-  if(!userId) {
+  if (!userId) {
     return redirect("/");
   }
 
@@ -25,56 +20,39 @@ const DonorIdPage = async ({
     country: " ",
     zipCode: " ",
     city: " ",
-    state: " "
-  }
+    state: " ",
+  };
 
-
-  return ( 
+  return (
     <>
       <div className="p-6">
         <div className="flex items-center justify-between">
           <div className="flex flex-col gap-y-2">
-            <h1 className="text-2xl font-medium">
-              Donor setup
-            </h1>
-            <span className="text-sm text-slate-700">
-            </span>
+            <h1 className="text-2xl font-medium">Donor setup</h1>
+            <span className="text-sm text-slate-700"></span>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
           <div className="space-y-4">
             <div>
               <div className="flex items-center gap-x-2">
-                <IconBadge icon={LayoutDashboard}/>
-                <h2 className="text-xl">
-                  Customize donor details
-                </h2>
+                <IconBadge icon={LayoutDashboard} />
+                <h2 className="text-xl">Customize donor details</h2>
               </div>
-              <NameForm
-                initialData={donor}
-                donorId={params.donorId}
-              />
-              <EmailForm
-                initialData={donor}
-                donorId={params.donorId}
-              />
+              <NameForm initialData={donor} donorId={params.donorId} />
+              <EmailForm initialData={donor} donorId={params.donorId} />
             </div>
           </div>
           <div>
-          <div className="flex items-center gap-x-2">
-              <IconBadge icon={Map}/>
-              <h2 className="text-xl">
-                Donor Address
-              </h2>
-            </div>
-            <div className="mt-6 border bg-slate-100 rounded-md p-4">
-              <GooglemapPlaces/>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Map} />
+              <h2 className="text-xl">Donor Address</h2>
             </div>
           </div>
         </div>
       </div>
     </>
-   );
-}
- 
+  );
+};
+
 export default DonorIdPage;
