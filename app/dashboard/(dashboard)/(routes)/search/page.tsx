2f04ba7562df9873b3422684796be30b6041b60a
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-09-24 20:04:09 +0100

    Fix type errors in SearchPage component

--- app/dashboard/(dashboard)/(routes)/search/page.tsx
+++ app/dashboard/(dashboard)/(routes)/search/page.tsx
@@ -5,19 +5,32 @@ import { db } from "@/lib/db";
 import { SearchInput } from "@/components/search-input";
 import { getCampaigns } from "@/actions/get-campaigns";
 import { CampaignsList } from "@/components/campaigns-list";
-
 import { Categories } from "./_components/categories";
 
 interface SearchPageProps {
   searchParams: {
     title: string;
     categoryId: string;
-  }
+  };
 }
 
-const SearchPage = async ({
-  searchParams
-}: SearchPageProps) => {
+interface CampaignWithProgressWithCategory {
+  id: string;
+  userId: string;
+  title: string;
+  description: string | null;
+  imageUrl: string | null;
+  fund: number | null;
+  isPublished: boolean;
+  categoryId: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  category: { id: string; name: string } | null;
+  campaigns: { id: string }[]; 
+  progress: number | null; 
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
   const { userId } = auth();
 
   if (!userId) {
@@ -26,31 +39,34 @@ const SearchPage = async ({
 
   const categories = await db.category.findMany({
     orderBy: {
-      name: "asc"
-    }
+      name: "asc",
+    },
   });
-  console.log("categories", categories)
+  console.log("categories", categories);
 
-  const campaigns = await getCampaigns({
+  const campaignsData = await getCampaigns({
     ...searchParams,
   });
 
-  return ( 
+  const campaigns: CampaignWithProgressWithCategory[] = campaignsData.map(
+    (campaign) => ({
+      ...campaign,
+      campaigns: [], 
+      progress: null, 
+    })
+  );
+
+  return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
-        <SearchInput/>
+        <SearchInput />
       </div>
       <div className="p-6 space-y-4">
-        {
-          campaigns.length !==0 &&  
-          <Categories
-            items={categories}
-          />
-        }
+        {campaigns.length !== 0 && <Categories items={categories} />}
         <CampaignsList items={campaigns} />
       </div>
     </>
-   );
-}
- 
+  );
+};
+
 export default SearchPage;
