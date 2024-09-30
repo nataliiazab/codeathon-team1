e0d6c633bfbdd179eb1fc1225b3b68bbf9fc7ad8
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-09-30 16:50:10 +0100

    improve spinner

--- app/(home)/_components/spinner.tsx
+++ app/(home)/_components/spinner.tsx
@@ -1,6 +1,10 @@
-const Spinner = () => (
-  <div className="flex justify-center items-center h-48">
-    <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-[#059669] border-t-2 border-transparent"></div>
+
+
+
+const Spinner: React.FC = () => (
+  <div className="flex flex-col justify-center items-center h-48">
+    <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-b-4 border-[#37AB87] border-solid"></div>
+    <p className="mt-2 text-gray-700">Loading campaigns...</p>{" "}
   </div>
 );
 
